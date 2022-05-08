90e4f2db65c1f5583f2ca3d49c5e7771718188b3
NomadNaomie <89711830+NomadNaomie@users.noreply.github.com>
2022-05-08 11:05:04 -0400

    Tone indicators are no longer hardcoded

--- ToneIndicator.plugin.js
+++ ToneIndicator.plugin.js
@@ -2,7 +2,7 @@
  * @name ToneIndicators
  * @author NomadNaomie, Zuri
  * @description Displays the messages tone indicators or by highlighting a tone tag will give you the defintion
- * @version 1.3.2
+ * @version 1.4.0
  * @source https://github.com/NomadNaomie/BD-Tone-Indicators
  * @updateUrl https://raw.githubusercontent.com/NomadNaomie/BD-Tone-Indicators/main/ToneIndicator.plugin.js
  * @authorId 188323207793606656, 746871249791221880
@@ -11,45 +11,8 @@
  */
 
 module.exports = (_ => {
-    const toneMap = [
-        { tag: ["/j", "joking"], colors: ['#BFFCC6', '#0d7a1a'] },
-        { tag: ["/hj", "half joking"], colors: ['#D8FFD6', '#0d7a1a'] },
-        { tag: ["/srs", "serious"], colors: ['#6EB5FF', '#29629e'] },
-        { tag: ["/nsrs", "not serious"], colors: ['#F6A6FF', '#a639b3'] },
-        { tag: ["/lh", "light hearted"], colors: ['#ACE6FF', '#217ca3'] },
-        { tag: ["/ij", "inside joke"], colors: ['#E7FFAC', '#6d9114'] },
-        { tag: ["/ref", "reference"], colors: ['#AFCBFF', '#144091'] },
-        { tag: ["/t", "teasing"], colors: ['#FFFFD1', '##c7c710'] },
-        { tag: ["/nm", "not mad"], colors: ['#DCD3FF', '#3d1bc2'] },
-        { tag: ["/lu", "a little upset"], colors: ['#FFF5BA', '#8f7c0b'] },
-        { tag: ["/nf", "not forced"], colors: ['#DEFDE0', '#089611'] },
-        { tag: ["/nbh", "nobody here"], colors: ['#FCF7DE', '#7a6600'] },
-        { tag: ["/nsb", "not subtweeting"], colors: ['#DEF3FD', '#00405e'] },
-        { tag: ["/nay", "not at you"], colors: ['#F0DEFD', '#7a02d1'] },
-        { tag: ["/nav", "not a vent"], colors: ['#00cc99', '#0aAB31'] },
-        { tag: ["/ay", "at you"], colors: ['#FDDFDF', '#8c0707'] },
-        { tag: ["/nbr", "not being rude"], colors: ['#9ADBB3', '#048a38'] },
-        { tag: ["/ot", "off-topic"], colors: ['#E7FFAC', '#476108'] },
-        { tag: ["/th", "threat"], colors: ['#FFABAB', '#b50e0e'] },
-        { tag: ["/cb", "clickbait"], colors: ['#F3FFE3', '#569106'] },
-        { tag: ["/f", "fake"], colors: ['#85E3FF', '#066480'] },
-        { tag: ["/c", "copypasta"], colors: ['#DBFFD6', '#1f8c0f'] },
-        { tag: ["/m", "metaphor / metaphorically"], colors: ['#FBE4FF', '#750987'] },
-        { tag: ["/li", "literal / literally"], colors: ['#BED2FE', '#0a328a'] },
-        { tag: ["/hyp", "hyperbole"], colors: ['#F6F9FF', '#2a3c5e'] },
-        { tag: ["/p", "platonic"], colors: ['#B5CCFE', '#2260e6'] },
-        { tag: ["/r", "romantic"], colors: ['#FFCCF9', '#bf19ac'] },
-        { tag: ["/a", "alterous"], colors: ['#C7D9FE', '#1458e3'] },
-        { tag: ["/neu", "neutral / neutral connotation"], colors: ['#E3ECFF', '#315ab0'] },
-        { tag: ["/g", "/gen", "/genq", "genuine / genuine question"], colors: ['#AFF8D8', '#089c5b'] },
-        { tag: ["/s", "/sarc", "sarcastic / sarcasm"], colors: ['#FFCBC1', '#cf3d21'] },
-        { tag: ["/l", "/ly", "lyrics"], colors: ['#97A2FF', '#0e1873'] },
-        { tag: ["/rt", "/rh", "rhetorical question"], colors: ['#A79AFF', '#1c099c'] },
-        { tag: ["/sx", "/x", "sexual intent"], colors: ['#FF9CEE', '#780b65'] },
-        { tag: ["/nsx", "/ns", "/nx", "non-sexual intent"], colors: ['#FFBEBC', '#7a201d'] },
-        { tag: ["/pc", "/pos", "positive connotation"], colors: ['#84D3B2', '#0e9e62'] },
-        { tag: ["/nc", "/neg", "negative connotation"], colors: ['#FFCCBB', '#87452f'] },
-    ].map(v => { let desc = v.tag.pop(); return [v.tag, desc, v.colors] })
+    var toneMap = []
+    !require("fs").existsSync(require("path").join(BdApi.Plugins.folder, "ToneIndicators.json")) ? require("request").get("https://raw.githubusercontent.com/NomadNaomie/BD-Tone-Indicators/main/ToneIndicators.json", (err, res, body) => {if(!err && res.statusCode === 200 && body){require("fs").writeFileSync(require("path").join(BdApi.Plugins.folder, "ToneIndicators.json"), body);toneMap = JSON.parse(body).toneMap.map(v => { let desc = v.tag.pop(); return [v.tag, desc, v.colors]})}}) : toneMap = JSON.parse(require("fs").readFileSync(require("path").join(BdApi.Plugins.folder, "ToneIndicators.json"))).toneMap.map(v => { let desc = v.tag.pop(); return [v.tag, desc, v.colors]})
     const findResults = (s, furtherSearch, directMatch) => {
         if (directMatch) return toneMap.filter(v => v[0].find(x => x === s))[0];
         let firstSearch = toneMap.filter(v => v[0].find(x => x.startsWith(s)));
@@ -68,28 +31,18 @@ module.exports = (_ => {
                 { name: 'NomadNaomie', discord_id: '188323207793606656', github_username: 'NomadNaomie', twitter_username: 'NomadNaomie' },
                 { name: 'Zuri', discord_id: '746871249791221880', github_username: 'Zuriix', website: "https://zuriix.github.io/" }
             ],
-            version: '1.3.2',
+            version: '1.4.0',
             description: 'Displays the messages tone indicators or by highlighting a tone tag will give you the defintion',
             github_raw: 'https://raw.githubusercontent.com/NomadNaomie/BD-Tone-Indicators/main/ToneIndicator.plugin.js',
             github: 'https://github.com/NomadNaomie/BD-Tone-Indicators'
         },
         changelog: [
-
+            { title: '1.4.0', type: "added", items: ['Tone tags are no longer hard coded, find ToneIndicators.json in your plugin folder to change them.'] },
             {
                 title: "1.3.2 - Fixed Autocomplete",
                 type: "fixed",
                 items: ["Fixed Autocomplete not working correctly"]
             },
-            // {
-            //     title: "1.3.0 - Autocomplete",
-            //     type: "added",
-            //     items: ["Added new Autocomplete for tone tags"]
-            // },
-            // {
-            //     title: "1.3.0 - CSS",
-            //     type: "improved",
-            //     items: ["A lot of code cleanup", "Custom CSS support (.tone-tag & .tone-tooltip)"]
-            // }
 
 
         ],
@@ -165,10 +118,10 @@ module.exports = (_ => {
     };
 
     if (!global.ZeresPluginLibrary) {
-        return class { load() { BdApi.showConfirmationModal("Zere's Library Missing", "Either Click Download Now to install it or manually install it. ", { confirmText: "Automatically Install", cancelText: "Cancel", onConfirm: () => { require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async(error, result, body) => {!error && result.statusCode == 200 && body ? require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, _ => BdApi.showToast("Finished downloading Zere's Plugin Library", { type: "success" })) : BdApi.showToast("Failed to download Zere's Plugin Library", { type: "error" }) }) } }) } };
+        return class { load() { BdApi.showConfirmationModal("Zere's Library Missing", "Either Click Download Now to install it or manually install it. ", { confirmText: "Automatically Install", cancelText: "Cancel", onConfirm: () => { require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, result, body) => { !error && result.statusCode == 200 && body ? require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, _ => BdApi.showToast("Finished downloading Zere's Plugin Library", { type: "success" })) : BdApi.showToast("Failed to download Zere's Plugin Library", { type: "error" }) }) } }) } };
     } else {
         return (([Plugin, Zlib]) => {
-            const { WebpackModules, ContextMenu, Patcher, showToast, React, clearCSS, injectCSS } = {...Zlib, ...BdApi };
+            const { WebpackModules, ContextMenu, Patcher, showToast, React, clearCSS, injectCSS } = { ...Zlib, ...BdApi };
             return class ToneIndicators extends Plugin {
                 generateBackgroundColor(r, e) { let n = "0x" + r.substring(1); return "rgba(" + [n >> 16 & 255, n >> 8 & 255, 255 & n].join(",") + `,${e})` }
                 getSettingsPanel() { const panel = this.buildSettingsPanel(); return panel.getElement(); }
@@ -210,15 +163,15 @@ module.exports = (_ => {
                         },
                         renderResults: data => {
                             return [React.createElement(Autocomplete.Title, { title: ["Tone Indicator"] }),
-                                data.results.ret.map((tag, i) => React.createElement(Autocomplete.Generic, {
-                                    index: i,
-                                    text: React.createElement('span', { style: { color: tag.color }, children: tag.name }),
-                                    description: React.createElement('span', { style: { color: tag.color }, children: tag.desc.join(' ') }),
-                                    tag: tag,
-                                    onClick: data.onClick,
-                                    onHover: data.onHover,
-                                    selected: data.selectedIndex === i,
-                                }))
+                            data.results.ret.map((tag, i) => React.createElement(Autocomplete.Generic, {
+                                index: i,
+                                text: React.createElement('span', { style: { color: tag.color }, children: tag.name }),
+                                description: React.createElement('span', { style: { color: tag.color }, children: tag.desc.join(' ') }),
+                                tag: tag,
+                                onClick: data.onClick,
+                                onHover: data.onHover,
+                                selected: data.selectedIndex === i,
+                            }))
                             ]
                         },
                         onSelect: data => { return data.options.insertText(data.results.ret[data.index].desc[0]) }
